e33104883365352756fe98abd6bcb099dae7aed3
Spenser Porter <Spenser.Porter@gmail.com>
2018-03-29 08:10:04 -0400

    Fixed some syntax and moved unit test to it's own thing

--- html-fetcher.py
+++ html-fetcher.py
@@ -1,9 +1,8 @@
-import unittest
 import urllib.request
-
 
 class HtmlFetcher(object):
     """Return HTTPResponse object"""
+    # Chrome version 60 user agent
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
 
     def __init__(self, number_of_tries=1, proxy=None):
@@ -11,8 +10,8 @@ class HtmlFetcher(object):
         self.proxy = proxy
 
     def get_html(self, url_string):
-        """Takes in a url string and returns html string if possible"""
-        # Chrome version 60 user agent
+        """Takes in a url string and returns HttpResponse object"""
+        
         header = {'User-Agent': HtmlFetcher.user_agent}
         req = urllib.request.Request(url_string, headers=header)
 
@@ -29,18 +28,15 @@ class HtmlFetcher(object):
                 return HttpResponse(code, content)
 
             except urllib.error.URLError as error:
-                if n == self.number_of_tries:
+                if n == self.number_of_tries + 1:
                     return HttpResponse(error.code, error.reason)
 
             except urllib.error.HTTPError as http_error:
-                if n == self.number_of_tries:
+                if n == self.number_of_tries + 1:
                     return HttpResponse(http_error.code, http_error.reason)
 
             except IOError:
-            	return "Bad Proxy?"
-
-
-        
+            	return HttpResponse(777, "Bad Proxy?")
 
 class HttpResponse(object):
     """Http response object with params code, content"""
@@ -48,12 +44,3 @@ class HttpResponse(object):
     def __init__(self, code, content):
         self.code    = code
         self.content = content
-
-class TestHtmlFetcher(unittest.TestCase):
-    def test(self):
-        self.assertEqual(HtmlFetcher().get_html("http://ec2-54-174-172-97.compute-1.amazonaws.com/").content, "oh god help")
-        self.assertEqual(HtmlFetcher().get_html("http://www.yahoo.com/dicks").code, 404)
-
-
-if __name__ == '__main__':
-     unittest.main()
