b4edf528dd41836359a126190d23fe09726ac399
Spenser Porter <Spenser.Porter@Gmail.com>
2018-03-29 14:28:26 -0400

    Changed according to Tedelen suggestions

--- TestHtmlFetcher.py
+++ TestHtmlFetcher.py
@@ -4,9 +4,10 @@ from html_fetcher import *
 class TestHtmlFetcher(unittest.TestCase):
     def test(self):
         self.assertEqual(HtmlFetcher().get_html("http://ec2-54-174-172-97.compute-1.amazonaws.com/").content, "oh god help")
+        self.assertEqual(HtmlFetcher().get_html("http://www.google.com").code, 200)
         self.assertEqual(HtmlFetcher().get_html("http://www.yahoo.com/dicks").code, 404)
-        self.assertEqual(HtmlFetcher(proxy = 'http://50.233.137.37:80').get_html("http://ec2-54-174-172-97.compute-1.amazonaws.com/").content, "oh god help")
-        self.assertEqual(HtmlFetcher(proxy = 'http://50.233.137.37:80').get_html("http://www.yahoo.com/dicks").code, 404)
+
+
 
 
 if __name__ == '__main__':
--- html_fetcher.py
+++ html_fetcher.py
@@ -4,11 +4,11 @@ class HtmlFetcher(object):
     """Return HTTPResponse object"""
     # Chrome version 60 user agent
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
-    timeout = 5
 
-    def __init__(self, number_of_tries=1, proxy=None):
+    def __init__(self, number_of_tries=1, proxy=None, timeout=5):
         self.number_of_tries = number_of_tries
-        self.proxy = proxy #proxy string in the form 'type://host:port'
+        self.proxy = Proxy(proxy) if proxy is not None else None #proxy string in the form 'type://host:port'
+        self.timeout = timeout
 
     def get_html(self, url_string):
         """Takes in a url string and returns HttpResponse object"""
@@ -17,8 +17,7 @@ class HtmlFetcher(object):
         req = urllib.request.Request(url_string, headers=header)
 
         if self.proxy is not None:
-            proxy = Proxy(self.proxy)
-            req.set_proxy(proxy.host, proxy.type)
+            req.set_proxy(self.proxy.host, self.proxy.type)
 
         for n in range(1, self.number_of_tries + 1):
             try:
@@ -28,15 +27,11 @@ class HtmlFetcher(object):
                 return HttpResponse(code, content)
 
             except urllib.error.URLError as error:
-                if n == self.number_of_tries:
-                    return HttpResponse(error.code, error.reason)
+                return HttpResponse(error.code, error.reason)
 
             except urllib.error.HTTPError as http_error:
                 if n == self.number_of_tries:
                     return HttpResponse(http_error.code, http_error.reason)
-
-            except IOError:
-                return HttpResponse(777, "Bad Proxy?")
 
 class HttpResponse(object):
     """Http response object with params code, content"""
@@ -48,8 +43,8 @@ class HttpResponse(object):
 class Proxy(object):
     """Takes a proxy string and parses into type and host"""
 
-    def __init__(self, proxy):
-        parsed        = urllib.parse.urlparse(proxy)
+    def __init__(self, proxy_url):
+        parsed        = urllib.parse.urlparse(proxy_url)
         self.type     = parsed.scheme
         self.host     = parsed.netloc
 
