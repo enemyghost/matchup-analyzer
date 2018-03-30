588e49ae7bb67394b9c041716cb1942405420d18
Spenser Porter <spenser.porter@gmail.com>
2018-03-30 12:47:01 -0400

    Proxy implemented, filename changed to avoid import conflicts (#3)
    * Proxy implimented, filename changed to avoid import conflicts
    
    * Changed according to Tedelen suggestions

--- TestHtmlFetcher.py
+++ TestHtmlFetcher.py
@@ -1,10 +1,13 @@
 import unittest
-import html-fetcher
+from html_fetcher import *
 
 class TestHtmlFetcher(unittest.TestCase):
     def test(self):
         self.assertEqual(HtmlFetcher().get_html("http://ec2-54-174-172-97.compute-1.amazonaws.com/").content, "oh god help")
+        self.assertEqual(HtmlFetcher().get_html("http://www.google.com").code, 200)
         self.assertEqual(HtmlFetcher().get_html("http://www.yahoo.com/dicks").code, 404)
+
+
 
 
 if __name__ == '__main__':
--- html_fetcher.py
+++ html_fetcher.py
@@ -1,42 +1,37 @@
-import urllib.request
+import urllib.request, urllib.parse
 
 class HtmlFetcher(object):
     """Return HTTPResponse object"""
     # Chrome version 60 user agent
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
 
-    def __init__(self, number_of_tries=1, proxy=None):
+    def __init__(self, number_of_tries=1, proxy=None, timeout=5):
         self.number_of_tries = number_of_tries
-        self.proxy = proxy
+        self.proxy = Proxy(proxy) if proxy is not None else None #proxy string in the form 'type://host:port'
+        self.timeout = timeout
 
     def get_html(self, url_string):
         """Takes in a url string and returns HttpResponse object"""
         
-        header = {'User-Agent': HtmlFetcher.user_agent}
+        header = {'User-Agent': self.user_agent}
         req = urllib.request.Request(url_string, headers=header)
 
         if self.proxy is not None:
-        	handler = urllib.request.ProxyHandler(proxy)
-        	opener = urllib.request.build_opener(handler)
-        	urllib.request.install_opener(opener)
+            req.set_proxy(self.proxy.host, self.proxy.type)
 
         for n in range(1, self.number_of_tries + 1):
             try:
-                response = urllib.request.urlopen(req)
-                content = response.read().decode(response.headers.get_content_charset())
-                code = response.code
+                response = urllib.request.urlopen(req, timeout=self.timeout)
+                content  = response.read().decode(response.headers.get_content_charset())
+                code     = response.code
                 return HttpResponse(code, content)
 
             except urllib.error.URLError as error:
-                if n == self.number_of_tries + 1:
-                    return HttpResponse(error.code, error.reason)
+                return HttpResponse(error.code, error.reason)
 
             except urllib.error.HTTPError as http_error:
-                if n == self.number_of_tries + 1:
+                if n == self.number_of_tries:
                     return HttpResponse(http_error.code, http_error.reason)
-
-            except IOError:
-            	return HttpResponse(777, "Bad Proxy?")
 
 class HttpResponse(object):
     """Http response object with params code, content"""
@@ -44,3 +39,12 @@ class HttpResponse(object):
     def __init__(self, code, content):
         self.code    = code
         self.content = content
+
+class Proxy(object):
+    """Takes a proxy string and parses into type and host"""
+
+    def __init__(self, proxy_url):
+        parsed        = urllib.parse.urlparse(proxy_url)
+        self.type     = parsed.scheme
+        self.host     = parsed.netloc
+
