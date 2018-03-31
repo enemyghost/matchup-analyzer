2aa27fbf57c92e0c07d06f81448717881b52c64c
Spenser Porter <Spenser.Porter@Gmail.com>
2018-03-31 11:26:01 -0400

    Updated file structure, renamed files, first draft of html_parser

---
+++ data-ingestion/html_parser.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+
+class GetSoup(object):
+    """Accepts html and outputs soup"""
+    def __init__(self, parser="lxml"):
+        self.parser = parser
+
+    def get_soup(self, raw_html):
+        return BeautifulSoup(raw_html, self.parser)
+
+
+class HtmlParser(object):
+    """Parses html soup for html_tag and optional class:value dict"""
+
+    def __init__(self, html_tag="tbody", class_dict={}):
+        self.html_tag = html_tag
+        self.class_dict = class_dict
+
+    def get_tables(self, soup):
+        find_tables = soup.find_all(self.html_tag, self.class_dict)
+        data_out = []
+        for table in find_tables:
+            table_data = []
+            if is_deepest_table(table, self.html_tag, self.class_dict):
+                rows = table.find_all('tr')
+                for row in rows:
+                    cols = row.find_all('td')
+                    table_data.append([ele.get_text(strip = True) for ele in cols])
+                data_out.append(table_data)
+
+        return data_out
+
+
+def is_deepest_table(table_soup, html_tag, class_dict):
+    if len(table_soup.find_all(html_tag, class_dict)) == 0:
+        return True
+    return False
+
+
+
+
+
+
