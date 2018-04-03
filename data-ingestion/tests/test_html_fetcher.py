62df238afa5f0df9977612ddffb18c8ff51e5e72
Spenser Porter <Spenser.Porter@Gmail.com>
2018-04-03 16:22:35 -0400

    line link crawler first commit

--- data-ingestion/tests/test_html_fetcher.py
+++ data-ingestion/tests/test_html_fetcher.py
@@ -3,7 +3,6 @@ import html_fetcher
 
 class TestHtmlFetcher(unittest.TestCase):
     def test(self):
-        self.assertEqual(HtmlFetcher().get_html("http://ec2-54-174-172-97.compute-1.amazonaws.com/").content, "oh god help")
         self.assertEqual(HtmlFetcher().get_html("http://www.google.com").code, 200)
         self.assertEqual(HtmlFetcher().get_html("http://www.yahoo.com/dicks").code, 404)
 
