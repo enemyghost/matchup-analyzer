ea65443663fe9d02b293008ab6dbf638a276984b
Tyler J Edelen <enemyghost@users.noreply.github.com>
2018-04-23 12:17:51 -0400

    write line movement URLs to postgres (#9)

---
+++ migrations/odds/001_init_schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS vendor (
+  vendor_id integer PRIMARY KEY,
+  vendor_name varchar(1024) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS sport (
+  sport_id integer PRIMARY KEY,
+  sport_name varchar(1024) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS sport_alias_by_vendor (
+  vendor_id integer REFERENCES vendor DEFAULT NULL,
+  sport_id integer REFERENCES sport,
+  sport_alias varchar(1024) NOT NULL,
+  PRIMARY KEY (vendor_id, sport_id)
+);
+
+CREATE TABLE IF NOT EXISTS line_url_scheduling (
+  normalized_url varchar(2000) PRIMARY KEY,
+  sport_id integer NOT NULL REFERENCES sport,
+  vendor_id integer NOT NULL REFERENCES vendor,
+  event_time_epoch_ms bigint
+);
