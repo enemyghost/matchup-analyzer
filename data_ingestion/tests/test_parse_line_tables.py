1c34fc48c623f021f2404ab90b4be29542e165d5
Spenser Porter <Spenser.Porter@gmail.com>
2018-05-28 19:36:39 -0400

    Added html_persister spider for saving urls html response text to a file, fixed error in parser unit test and added comparison between html parsers for further testing

--- data_ingestion/tests/test_parse_line_tables.py
+++ data_ingestion/tests/test_parse_line_tables.py
@@ -2,23 +2,24 @@ import unittest
 import sys, os
 from data_ingestion.crawlers.spiders import parse_line_tables as parser
 from bs4 import BeautifulSoup as bs
-import urllib
+import scrapy
 
-test_file_path = "data_ingestion/tests/fixtures/cavaliers_at_celtics_line.html"
-#"data_ingestion/tests/fixtures/rockets_at_warriors_line_movements.htm"
+test_file_path = "data_ingestion/tests/fixtures/cavs_at_celts_request.html"
 
 class Test_LineTableParser(unittest.TestCase):
+    parsers = ["lxml", "html5lib", "html.parser"]
+
     def test_LineTableParser(self):
-        with open(test_file_path, 'r', encoding='utf-8') as test_html:
-        #with open(test_file_path) as test_html:
-            #test_html = test_html_file.read()
-            soup_test = bs(test_html.read(), "html5lib")
-            print(len(soup_test.find_all("tbody")))
+        for parser in self.parsers:
+            with open(test_file_path) as test_html_file:
+
+                test_html_bytes = test_html_file.read().encode('utf-8')
+                soup = bs(test_html_bytes, parser)
+                tables = soup.find_all('tbody')
+                print(parser, "found:", len(tables), "tables")
 
             # test_parser = parser.LineTableParser()
             # output = test_parser.get_tables(test_html)
-
-
             #print(output)
 
 
